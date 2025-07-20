c1b4c6e64b83a775a45beaa2044253c18f289288
sabarim6369 <sabarim6369@gmail.com>
2025-07-21 01:57:16 +0530

    minimal changes

--- Routers/userrouter.go
+++ Routers/userrouter.go
@@ -2,7 +2,7 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/sabarim6369/Ginframework_golang/controllers"  // replace "your-module-name" with your actual module name
+	"github.com/sabarim6369/Ginframework_golang/controllers"  
 )
 
 func UserRoutes(r *gin.Engine) {
